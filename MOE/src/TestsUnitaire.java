1f01986c2fd03f6fb411720109050478444ab13a
unknown <ramoni@B-0-12-04.iutparis.local>
2016-01-12 15:52:08 +0100

    Test Conversion

--- MOE/src/TestsUnitaire.java
+++ MOE/src/TestsUnitaire.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -7,8 +10,21 @@ import org.junit.Test;
 public class TestsUnitaire {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TestStringToCombinaison() {
+		String s = "rvvb";
+		StringToCombinaisonConverter converteur = new StringToCombinaisonConverter(s);
+		ArrayList<Color> a = new ArrayList<Color>();
+		a.add(0, Color.Red);
+		a.add(1, Color.Green);
+		a.add(2, Color.Green);
+		a.add(3, Color.Blue);
+
+		Combinaison get = converteur.conversion();
+		ArrayList<Color> b = get.getTabCombinaison();
+		
+		Assert.assertEquals(a,b);
+
+		
 	}
 
 }
