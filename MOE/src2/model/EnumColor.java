2662653a1f9fabfaeba7ea0c2687940147f26303
unknown <colonna1@B-0-12-06.iutparis.local>
2016-01-26 16:24:10 +0100

    Ajout Javadoc :
    -Mastermind.java
    -Combinaison.java
    -EnumColor.java

--- MOE/src2/model/EnumColor.java
+++ MOE/src2/model/EnumColor.java
@@ -4,7 +4,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-
+/**
+ * Classe représentant une couleur
+ * 
+ * @author Alexis Colonna
+ *
+ */
 public enum EnumColor {
 
 	//Objets directement construits
@@ -17,14 +22,25 @@ public enum EnumColor {
 	private int Number = 0;
 	private char Character = '.';
 
-	//Constructeur
+	
 
+	/**
+	 * Constructeur de l'enum
+	 * 
+	 * @param number numéro représentant la couleur à créer
+	 * @param character lettre représentant la couleur à créer
+	 */
 	EnumColor(int number, char character){
 		this.Number = number;
 		this.Character = character;
 
 	}
 	
+	/**
+	 * Constructeur de la couleur à partir d'une couleur préexistante
+	 * 
+	 * @param c Une instance d'EnumColor
+	 */
 	EnumColor(EnumColor c){
 		this.Number = c.getNumber();
 		this.Character =c.getChar();
@@ -36,14 +52,27 @@ public enum EnumColor {
 	private static final int SIZE = VALUES.size();
 	private static final Random RANDOM = new Random();
 
+	/**
+	 * Permet de choisir une couleur au hasard
+	 * @return une couleur de type enumColor parmis celles stockées dans la liste VALUES
+	 */
 	public static EnumColor randomEnumColor(){
 		return VALUES.get(RANDOM.nextInt(SIZE));
 	}
 	
+	/**
+	 * Permet de récupérer la liste des couleurs
+	 * @return liste des couleurs
+	 */
 	public static List<EnumColor> allEnumColor(){
 		return VALUES;
 	}
 	
+	/**
+	 * Permet de récupérer une couleur à partir de la lettre qui la représente
+	 * @param c Lettre représentant la couleur 
+	 * @return La couleur demandée si elle existe ou null
+	 */
 	public static EnumColor getEnumColorWithChar(char c){
 		for (EnumColor EnumColor : VALUES) {
 			if(EnumColor.getChar() == c){
@@ -53,10 +82,18 @@ public enum EnumColor {
 		return null;
 	}
 
+	/**
+	 * 
+	 * @return Nombre repésentant la couleur
+	 */
 	int getNumber(){
 		return this.Number;
 	}
 	
+	/**
+	 * 
+	 * @return La lettre représentant la couleur
+	 */
 	char getChar(){
 		return this.Character;
 	}
